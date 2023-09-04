435fe7b38d453c09bdeca8bd8654efff1b8f3e4e
AlejoHexacto <castiblancosolerd@gmail.com>
2023-09-04 12:55:11 -0500

    fix swagger
    swagger documentation

--- ProductManagement.Api/Startup.cs
+++ ProductManagement.Api/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +40,15 @@ namespace ProductManagement.Api
                 .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Implement Swagger UI",
+                    Description = "A simple example to Implement Swagger UI",
+                });
+            });
             services.AddScoped<IProduct, ProductDomain>();
             services.AddScoped<IProductUseCase, Product>();
             services.AddScoped<IProductRepository, ProductRepository>();
